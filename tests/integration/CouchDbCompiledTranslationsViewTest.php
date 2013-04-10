069a0c6d41364dfd977db9bde14374dc79d471dd
Magomogo <mgnatenko@gmail.com>
2013-04-10 08:52:34 +0700

    Test for pluggable locale settings

--- tests/integration/CouchDbCompiledTranslationsViewTest.php
+++ tests/integration/CouchDbCompiledTranslationsViewTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Translator\Storage\CouchDb;
 use Translator\String;
 use Translator\Test\CouchDbTestCase;
 use Translator\Storage\CouchDb\Schema;
@@ -13,8 +14,9 @@ class CouchDbCompiledTranslationsViewTest extends CouchDbTestCase
         $http = new HttpClient();
 
         $response = $http->request('GET', '/' . TEST_COUCHDB_NAME . '/_design/main/_list/compiled/translations', null, true);
+
         $this->assertEquals(
-            <<<JS
+            <<<'JS'
 (function(g){g.i18n = {};
 g.i18n['validation'] = {};
 g.i18n['validation']['email'] = function(d){
@@ -27,5 +29,18 @@ JS
             ,
             $response->body
         );
+    }
+
+    public function testLoadsCorrectLocaleSettings()
+    {
+        $ruStorage = new CouchDb(self::db(), 'ru_RU');
+
+        $ruStorage->registerString(String::create('num', '{NUM, plural, one: {одна овца} few {# овцы} other {# овец}}'));
+        $http = new HttpClient();
+
+        $response = $http->request('GET', '/' . TEST_COUCHDB_NAME . '/_design/main/_list/compiled/translations', null, true);
+
+        $this->assertContains('MessageFormat.locale["ru"]', $response->body);
+
     }
 }
