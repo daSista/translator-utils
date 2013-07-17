20f60fc8d50b527e787e4f3091a1e2f1bc0741ec
Magomogo <mgnatenko@gmail.com>
2013-07-17 15:59:58 +0700

    + Implemented mustache view translation clause parser

---
+++ class/Translator/SourceCode/MustacheViewClause.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Translator\SourceCode;
+
+class MustacheViewClause
+{
+    private $firstPart;
+    private $paramPart;
+
+    public function __construct($string)
+    {
+        $parts = preg_split('/^([^ ]+) /', $string, -1, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY) + array('', '');
+        $this->firstPart = $parts[0];
+        $this->paramPart = $parts[1];
+    }
+
+    public function keyWithNamespace()
+    {
+        return $this->firstPart;
+    }
+
+    public function parameters()
+    {
+        $parts = preg_split(
+            '/\\s*([a-zA-Z0-9]+)\\s*=\\s*/',
+            $this->paramPart,
+            -1,
+            PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY
+        );
+
+        $nameToValueMap = array();
+        while(count($parts)) {
+            $name = array_shift($parts);
+            $value = array_shift($parts);
+            $nameToValueMap[$name] = substr($value, 1, -1);
+        }
+
+        return $nameToValueMap;
+    }
+}
---
+++ tests/unit/Translator/SourceCode/MustacheViewClauseTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Translator\SourceCode;
+
+class MustacheViewClauseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider testCases
+     */
+    public function testExtractsKeyWithNamespaceAndParametersMap($expectedKeyWithNs, $expectedParams, $mustacheClause)
+    {
+        $clause = new MustacheViewClause($mustacheClause);
+
+        $this->assertSame($expectedKeyWithNs, $clause->keyWithNamespace());
+        $this->assertSame($expectedParams, $clause->parameters());
+    }
+
+    public static function testCases()
+    {
+        return array(
+            array('', array(), ''),
+            array('page', array(), 'page'),
+            array('meta/index:keywords', array(), 'meta/index:keywords'),
+            array(
+                'meta/city:keywords',
+                array('city' => 'Lugano', 'region' => 'Ticino'),
+                'meta/city:keywords city="Lugano" region="Ticino"'
+            ),
+            array(
+                'xHotelsFound',
+                array('NUM' => '9'),
+                'xHotelsFound NUM = "9"'
+            ),
+            array(
+                'helloPerson',
+                array('NAME' => 'John Doe'),
+                'helloPerson NAME = "John Doe"'
+            ),
+            array(
+                'names:caffee',
+                array('TITLE' => 'Legendary "Titanic"'),
+                'names:caffee TITLE = "Legendary "Titanic""'
+            ),
+            array(
+                'greetVisitor',
+                array('NAME' => ''),
+                'greetVisitor NAME=""'
+            ),
+            array(
+                'greetVisitor',
+                array('NAME' => '', 'TITLE' => 'Mr.'),
+                'greetVisitor NAME ="" TITLE = "Mr."'
+            ),
+        );
+    }
+}
